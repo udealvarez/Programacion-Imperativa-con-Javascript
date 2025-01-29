3327872c34b8ebb3f5b72073c6bbf286302f0938
Eduardo AlvareZ <udealvarez@icloud.com>
2025-01-29 15:40:25 -0300

    Variables en js

---
+++ 1-Entorno/1-Variables-y-Tipo-de-datos/Variables.js
@@ -0,0 +1,16 @@
+let numero1;
+
+numero1 = 10; //asignacion de una variable - le asigno 10 a la variable numero1
+
+let numero2 = 14; //creamos variable y le asignamos un numero
+
+let numero3;
+
+console.log("Bienvenidos!");
+
+console.log(numero1);
+
+console.log(numero2);
+
+console.log(numero3); // en la terminar va a salir INDEFINED y sig que aun no se le asigno un valor pero si en el futuro
+                    // NULL lo voy a indicar cuando una variable no tiene ningun valor
