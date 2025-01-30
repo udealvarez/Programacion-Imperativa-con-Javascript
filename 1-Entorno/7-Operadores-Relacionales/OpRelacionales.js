62dae0a3e438f74a83edd7a8b9e3d1546677e04a
Eduardo AlvareZ <udealvarez@icloud.com>
2025-01-30 20:28:56 -0300

    Operacion de igualdad / igualdad estricta

---
+++ 1-Entorno/7-Operadores-Relacionales/OpRelacionales.js
@@ -0,0 +1,8 @@
+let numero1 = 10;
+let numero2 = 5;
+
+let resultado = numero1 > numero2;
+console.log(resultado);
+
+// el == operacion de IGUALDAD pero no compara el tipo de dato
+// igualacion ESTRICTA ===
