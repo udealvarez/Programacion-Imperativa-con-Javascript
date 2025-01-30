b4151424ec4612d81c3cf150ec0035132d58fd3d
Eduardo AlvareZ <udealvarez@icloud.com>
2025-01-30 19:36:56 -0300

    Variables - Resta / Suma / Multiplicacion / Division / Modulo

---
+++ 1-Entorno/5-Operadores-Aritmeticos/Operadores.js
@@ -0,0 +1,27 @@
+// Variables
+let resultadoResta;
+let resultadoMultiplicacion;
+let nro = 15;
+let resultadoDivision;
+let resultadoSuma;
+let resultadoModulo
+
+// Resta
+resultadoResta = 10 - 5;
+console.log("El resultado de la resta fue: " + resultadoResta);
+
+// Multiplicacion
+resultadoMultiplicacion = nro * nro;
+console.log("El resultado de la multiplicacion es: " + resultadoMultiplicacion)
+
+// Division
+resultadoDivision = 100 / 2;
+console.log("El resultado de la division es: " + resultadoDivision);
+
+// Suma
+resultadoSuma = 10 + 10.5;
+console.log("El resultado de la suma es: " + resultadoSuma);
+
+// Modulo
+resultadoModulo = 11 % 2;
+console.log("El resto del modulo es: " + resultadoModulo);
