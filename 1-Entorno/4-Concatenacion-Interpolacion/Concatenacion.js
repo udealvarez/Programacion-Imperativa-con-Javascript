260e78791b5db545c5249d3d3b06ccd5fc1b5a9c
Eduardo AlvareZ <udealvarez@icloud.com>
2025-01-29 23:42:41 -0300

    Concatenacion e Interpolacion

---
+++ 1-Entorno/4-Concatenacion-Interpolacion/Concatenacion.js
@@ -0,0 +1,9 @@
+let nombre = "Eduardo";
+let apellido = "Alvarez";
+
+//esto es un ejemplo de concatenacion
+console.log("Hola! Mi nombre es: " + nombre + " " + apellido);
+
+//esto es un ejemplo de interpolacion
+console.log(`Hola! Mi nombre es: ${nombre} ${apellido}`);
+
