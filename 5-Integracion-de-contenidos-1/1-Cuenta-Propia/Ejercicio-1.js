8b13565d5e02c18bfa959d29d70a05c1d8a0ce65
Eduardo AlvareZ <udealvarez@icloud.com>
2025-02-04 13:13:57 -0300

    Enunciado en el ejercicio 1

--- 5-Integracion-de-contenidos-1/1-Cuenta-Propia/Ejercicio-1.js
+++ 5-Integracion-de-contenidos-1/1-Cuenta-Propia/Ejercicio-1.js
@@ -1,3 +1,10 @@
+/*
+Validador de contraseña: Crea una variable llamada ‘contraseña’ donde guardes un valor específico. 
+Luego, crea una función que reciba por parámetro un dato y utilizando operadores lógicos y condicionales,
+verifica si dicho parámetro coincide con la variable llamada ‘contraseña’. 
+La función deberá mostrar por consola si la contraseña recibida es válida o no.
+*/
+
 let contrasena = "junior";
 
 function validar(cadena){
@@ -10,3 +17,4 @@ function validar(cadena){
 
 validar("junior");
 validar("pepe");
+validar("john");
