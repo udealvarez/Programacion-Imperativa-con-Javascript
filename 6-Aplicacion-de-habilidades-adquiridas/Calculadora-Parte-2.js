50d6a76b67102f74055dba7857b3b754d913c272
Eduardo AlvareZ <udealvarez@icloud.com>
2025-02-13 13:02:27 -0300

    Refactor de Calculadora - Parte 1 | Parte 2 B - Final

--- 6-Aplicacion-de-habilidades-adquiridas/Calculadora-Parte-2.js
+++ 6-Aplicacion-de-habilidades-adquiridas/Calculadora-Parte-2.js
@@ -1,32 +1,96 @@
+const prompt = require("prompt-sync")({ sigint: true }); // este comando va al inicio del archivo
+
+/*
+    NOTA
+
+    Para cada archivo vamos a tener que instalar por la consola el siguiente comand
+
+    npm i prompt-sync       --> comando
+
+*/
+
+/*  algo que despues nos sirve mas abajo
+
+let numero = parseInt(prompt("Por favor ingrese un numero: ")); // con el parseInt parseo el numero a entero, sino tengo el parseInt me lo toma como string
+console.log("El numero que ha seleccioado es: " + numero);
+
+*/
+
 function suma(numero1, numero2){
 
     let resultado = numero1 + numero2;
     return resultado;
 }
 
-function resta(num1, num2){
+function resta(numero1, numero2){
 
-    let resultado = num1 - num2;
+    let resultado = numero1 - numero2;
     return resultado; 
 }
 
-function multiplicar(number1, number2){
+function multiplicar(numero1, numero2){
 
-    return number1 * number2;  
+    return numero1 * numero2;  
 }
 
-function division(numerito1, numerito2){
+function division(numero1, numero2){
 
-    if (numerito2 == 0) {
+    if (numero2 == 0) {
         return "NO SE PUEDE DIVIDIR POR CERO";
     }else{
-        return numerito1 / numerito2;
+        return numero1 / numero2;
     }
 }
 
+
 console.log("Bienvenido a la calculadora virtual");
-console.log("Indique que operacion quiere realizar:");
 console.log("1. Suma");
 console.log("2. Resta");
 console.log("3. Multiplicacion");
 console.log("4. Division");
+
+let opcion = parseInt(prompt("Indique que operacion quiere realizar: ")); // al agregarle el parseInt el prompt tiene que recibir un entero para entrar en el switch
+
+let numero1;
+let numero2;
+let resultado;
+
+switch (opcion) {
+    case 1:
+        numero1 = parseInt(prompt("Indique el primer numero: "));
+        numero2 = parseInt(prompt("Indique el segundo numero: "));
+        resultado = suma(numero1, numero2);
+        console.log("El resultado de la suma es: " + resultado);
+        break;
+
+    case 2:
+        numero1 = parseInt(prompt("Indique el primer numero: "));
+        numero2 = parseInt(prompt("Indique el segundo numero: "));
+        resultado = resta(numero1, numero2);
+        console.log("El resultado de la resta es: " + resultado);
+        break;
+
+    case 3:
+        numero1 = parseInt(prompt("Indique el primer numero: "));
+        numero2 = parseInt(prompt("Indique el segundo numero: "));
+        resultado = multiplicar(numero1, numero2);
+        console.log("El resultado de la multiplicacion es: " + resultado);
+        break;
+
+    case 4:
+        numero1 = parseInt(prompt("Indique el primer numero: "));
+        numero2 = parseInt(prompt("Indique el segundo numero: "));
+        resultado = division(numero1, numero2);
+
+        if (typeof resultado == "string") {
+            console.log(resultado);
+        }else{
+            console.log("El resultado de la division es: " + resultado);
+        }
+
+        break;
+        
+    default:
+        console.log("Ingreso una opcion no valida");
+        break;
+}
