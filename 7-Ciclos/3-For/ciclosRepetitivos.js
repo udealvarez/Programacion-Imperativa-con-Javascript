c4d1ee40b6581423b59fcea607355e6c0fe767ec
Eduardo AlvareZ <udealvarez@icloud.com>
2025-02-13 16:15:06 -0300

    Ciclos -> For

---
+++ 7-Ciclos/3-For/ciclosRepetitivos.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+
+for (let i = 0; i <= 10; i++) { // si quiero que el incremento vaya de a 2 --> i=+2
+                                // si quiero que disminuya --> i--
+
+    console.log("Iteracion numero: " + i);    
+}
