8dd8ed49305796c7dd95013625e33ddcc31990a7
Eduardo AlvareZ <udealvarez@icloud.com>
2025-02-17 16:33:20 -0300

    Objetos literales | Modelando la vida real

---
+++ 9-Modelando-Vida-Real/objetos.js
@@ -0,0 +1,19 @@
+// crear un objeto
+let persona = {     //al abrir { } objeto literal
+    nombre : "Eduardo",
+    apellido : "Alvarez",       // PROPIEDADES
+    edad : 36
+};
+
+
+// acceder a las propiedades de un objeto
+
+console.log(persona.nombre); // para ingresar a las propiedades, escribo un punto " . "
+
+
+
+// acceder a un metodo de un objeto
+
+
+
+// this
