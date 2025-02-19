a14fd4216a14244d63e143933f957dfe0a88b950
Eduardo AlvareZ <udealvarez@icloud.com>
2025-02-19 15:52:35 -0300

    Ejercicio 2  |  Lucrecia

---
+++ 10-Integracion-de-contenidos-2/2-Resolucion-Lucrecia/Ejercicio-1.js
@@ -0,0 +1,19 @@
+/*
+
+        Unir dos arrays: 
+
+Crea una función llamada ‘unirArrays’ que tome dos arrays como parámetros y los una en uno solo. 
+La función debe devolver el nuevo array.Para esto, averiguar qué hace el método .concat()
+
+*/
+
+
+function unirArrays(array1, array2){
+    return array1.concat(array2); // el metodo concat retorna un nuevo array uniendo 2 arrays
+                                // Array1 CONCATENA array2
+}
+
+let arreglo = [4,5,6];
+let resultadoDeUnion = unirArrays([1,2,3], arreglo);
+
+console.log(resultadoDeUnion);
